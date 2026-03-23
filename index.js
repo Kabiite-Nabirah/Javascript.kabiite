8c76f73ac57a576ca516e9bb0940a7bf219af5fa
Kabiite-Nabirah <kabiitenn@gmail.com>
2026-03-23 12:14:45 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+console.log("My name is Nabirah");
+
+// Variables
+// Declaring variables uses the var, let, or const keywords
+
+// var is function-scoped and can be redeclared and updated
+var name = "Nabirah";
+console.log(name);
+name = "Nabirah Kabiite";
+console.log(name);
+
+// let is block-scoped and can be updated but not redeclared
+let age = 25;
+console.log(age);
+age = 18;g
+console.log("My age has changed because I am just a girl", age);
+
+// const is block-scoped and cannot be updated or redeclared
+const country = "Uganda";
+console.log(country);
+country = "Kenya"; // This will throw an error because const cannot be reassigned
+
+// Data Types
+// JavasScript has several data types including:
+//1. String
+let greeting = "Hello, How are you?"
+console.log(greeting);
+console.log(typeof greeting);
+
+// 2. Number
+let x = 10;
+let y = "12"
+// console.log(x);
+console.log(y);
+// console.log(typeof x);
+console.log(typeof y);
